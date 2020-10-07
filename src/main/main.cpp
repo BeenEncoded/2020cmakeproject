718ebd25efbaffce6b3efd04b543689410ede2fd
Jonathan Whitlock <beenencoded@outlook.com>
2020-10-06 21:08:57 -0400

    a cmakelists for 2020

---
+++ src/main/main.cpp
@@ -0,0 +1,14 @@
+﻿/** Just a test program to make sure the toolset currently installed on the system works.*/
+#include <iostream>
+
+using namespace std;
+
+int main(int, char**)
+{
+	cout<< "Hello World!"<< endl;
+	for (unsigned int x = 0; x < 50; ++x)
+	{
+		std::cout<< "X = " << (x + 1) << std::endl;
+	}
+	return 0;
+}
